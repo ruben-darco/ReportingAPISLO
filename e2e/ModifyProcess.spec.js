6511c8c81278df638937761f5fec28763da524a7
Ruben d'Arco <ruben.darco@nintex.com>
2024-11-04 15:27:28 +0000

    Various

--- e2e/ModifyProcess.spec.js
+++ e2e/ModifyProcess.spec.js
@@ -11,35 +11,30 @@ import { test, expect, request } from "@playwright/test";
 
 test("Update Process Title and First Activity", async ({ page }) => {
   await page.goto("https://us.promapp.com/smoketest/home/login?returnurl=%2fsmoketest%2f");
-  await page.getByPlaceholder("Username").click();
-  await page.getByPlaceholder("Username").fill("Ruben");
-  await page.getByPlaceholder("Username").press("Tab");
-  await page.getByPlaceholder("Password").click();
-  await page.getByPlaceholder("Password").fill("wrAyH4qMwHif7r1sSAHnwGaYKiLNYyEY0M8t02Vik0Qh0Rg6gCjm");
+  await page.getByPlaceholder("Username").focus().fill("Ruben");
+  await page.getByPlaceholder("Password").focus().fill("wrAyH4qMwHif7r1sSAHnwGaYKiLNYyEY0M8t02Vik0Qh0Rg6gCjm");
   await page.getByRole("button", { name: "Login", exact: true }).click();
 
   // check if we're logged in
   await page.getByRole("link", { name: "Admin" }).isVisible();
 
-  const curDateTime = new Date().toISOString().substring(0, 19); // '2024-11-04T14:51:16'
+  const curDateTime = new Date().toISOString().substring(0, 19); // '2024-11-04T14:50:30
   const teststring = `RDA_${curDateTime}`;
 
   await page.goto("https://us.promapp.com/smoketest/Process/03fe63d3-8ecb-4a86-909c-3cd6228f796f");
   await page.getByRole("link", { name: "Edit" }).click();
 
-  //update the first activity
-  await page.locator('[data-test="process-activity-header-default"]').click();
-  await page.waitForSelector('[data-test="process-activity-header-edit"]');
-  await page.locator('[data-test="process-activity-header-edit"]').click();
-  await page.locator('[data-test="process-activity-header-edit"]').fill(teststring);
-  await page.locator('[data-test="process-procedure-commit"]').click();
-  await page.locator('[data-test="process-quick-save"]').click();
+  // //update the first activity
+  // await page.locator('[data-test="process-activity-header-default"]').click();
+  // await page.waitForSelector('[data-test="process-activity-header-edit"]');
+  // await page.locator('[data-test="process-activity-header-edit"]').click();
+  // await page.locator('[data-test="process-activity-header-edit"]').fill(teststring);
+  // await page.locator('[data-test="process-procedure-commit"]').click();
+  // await page.locator('[data-test="process-quick-save"]').click();
 
   // update the process title
   await page.locator("#pvm-app-container").getByText("Summary").click();
-  await page.locator('[data-test="process-edit-summary-title"]').click();
-  await page.locator('[data-test="process-edit-summary-title"]').focus();
-  await page.locator('[data-test="process-edit-summary-title"]').fill(teststring);
+  await page.locator('[data-test="process-edit-summary-title"]').focus().fill(teststring);
   await page.locator('[data-test="process-owner-autocomplete-container"] div').first().click();
   await page.locator('[data-test="process-quick-save"]').click();
 });
@@ -50,16 +45,12 @@ test("Check API for last item", async ({ request }) => {
       Authorization: "Bearer gxnqaVeXmyc9PgyagS02GWvVh1TUMv70mNMfkPBbMN4",
     },
   });
-
-  console.log(response);
-  console.log(response.body);
   expect(response.ok()).toBeTruthy();
-
   const jsonData = await response.json();
-  const proc = jsonData.value[0]
-  const compareValue = "RDA_"+process.env.LAST_RUN
-  console.log(`Process name is: ${proc.Name}`)
-  console.log(`Compare value is: ${compareValue}`)
-  expect(proc.Name == compareValue).toBeTruthy()
-  
+  const proc = jsonData.value[0];
+  const dateStr = proc.Name.substring(4); // strip off the 'RDA_'
+  let prevRun = new Date(Date.parse(dateStr))
+  const now = new Date()
+  const twelveHours = 14*3600000;
+  expect((dateStr - now) < twelveHours).toBeTruthy()
 });
